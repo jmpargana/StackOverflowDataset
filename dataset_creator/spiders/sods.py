473c576c98eb4b64a9f1b998dfcbd89d2560c29a
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-04-17 16:46:08 +0200

    saving questions as items without tracking

--- dataset_creator/spiders/sods.py
+++ dataset_creator/spiders/sods.py
@@ -3,24 +3,30 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from dataset_creator.items import QuestionItem
+
+import re
+
 
 class SodsSpider(CrawlSpider):
     name = 'sods'
-    allowed_domains = ['stackoverflow.com']
-    start_urls = ['https://stackoverflow.com/']
 
-    custom_settings = {
-        'LOG_FILE': 'logs/stackoverflowdataset.log',
-        'LOG_LEVEL': 'DEBUG'
-    }
+    # allowed_domains = ['stackoverflow.com']
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    )
+    start_urls = ['http://stackoverflow.com/questions?pagesize=50&sort=newest']
 
-    def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+    # rules = (
+    #     Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+    # )
+
+    def parse(self, response):
+        for question in response.css('div.question-summary'):
+            item = QuestionItem()
+
+            item['votes'] = int(question.css('div.votes strong::text').get())
+            item['answers'] = int(question.css('div.status strong::text').get())
+            item['views'] = int(re.findall(r'\d+', question.css('div.views::text').get())[0])
+            item['question'] = question.css('div.summary>h3>a::text').get()
+
+            yield item
+
