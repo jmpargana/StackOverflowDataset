d2b9bbd0ebfd8b9e658ed294f1bfaa0395a18bfe
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-04-17 17:04:52 +0200

    preparing mongodb

--- dataset_creator/spiders/sods.py
+++ dataset_creator/spiders/sods.py
@@ -1,32 +1,28 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-
 from dataset_creator.items import QuestionItem
-
-import re
 
 
 class SodsSpider(CrawlSpider):
-    name = 'sods'
-
-    # allowed_domains = ['stackoverflow.com']
-
-    start_urls = ['http://stackoverflow.com/questions?pagesize=50&sort=newest']
-
-    # rules = (
-    #     Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
-    # )
+    name = "sods"
+    start_urls = ["http://stackoverflow.com/questions?pagesize=50&sort=newest"]
 
     def parse(self, response):
-        for question in response.css('div.question-summary'):
+        for question in response.css("div.question-summary"):
             item = QuestionItem()
 
-            item['votes'] = int(question.css('div.votes strong::text').get())
-            item['answers'] = int(question.css('div.status strong::text').get())
-            item['views'] = int(re.findall(r'\d+', question.css('div.views::text').get())[0])
-            item['question'] = question.css('div.summary>h3>a::text').get()
+            item["votes"] = int(question.css("div.votes strong::text").get())
+            item["answers"] = int(question.css("div.status strong::text").get())
+            item["views"] = int(
+                re.findall(r"\d+", question.css("div.views::text").get())[0]
+            )
+            item["question"] = question.css("div.summary>h3>a::text").get()
 
             yield item
 
+        next_page = response.css("div.s-pagination>a.s-pagination--item").getall()[-1]
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
